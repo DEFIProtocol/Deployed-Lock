5ecc7341978490909d389eb12e162db1ef58c048
DEFIProtocol <92906608+DEFIProtocol@users.noreply.github.com>
2022-10-17 15:40:04 -0500

    Update ETHDEX.jsx

--- src/components/ETHDEX.jsx
+++ src/components/ETHDEX.jsx
@@ -1,5 +1,5 @@
 // favorites list into one component
-
+import "./tokenIndex.css"
 import { Link } from "react-router-dom";
 import { Card } from "antd";
 import { StarOutlined, StarFilled } from "@ant-design/icons";
@@ -72,142 +72,67 @@ function ETHDEX() {
   //};
 
   return (
-    <div
-      style={{
-        width: "100%",
-        padding: "15px",
-        alignItems: "center",
-        alignSelf: "center",
-      }}
-    >
+    <div className="ETHDEX">
       <div style={{ textAlign: "center" }}>
         <input
           placeholder="Search..."
-          style={{
-            margin: "40px auto",
-            backgroundColor: "#202020",
-            color: "#909090",
-            textAlign: "center",
-            lineHeight: "2em",
-          }}
+          className="searchBar"
           type="text"
           onChange={(e) => setQuery(e.target.value)}
         />
       </div>
-      <h1 style={{ color: "lime", textAlign: "center" }}>Tokens</h1>
-      <div style={{ color: "lime" }}>
-        <span style={{ float: "left", marginRight: "30%", marginLeft: "10%" }}>
-          DAO
-        </span>
-        <span style={{ float: "left", marginRight: "25%" }}>Type</span>
+      <h1 className="heading">Tokens</h1>
+      <div className="text">
+        <span className="column1">DAO</span>
+        <span className="column2">Type</span>
         <span>Price</span>
       </div>
       <div>
         {!tokens
           ? null
           : tokens
-              .filter((val) => {
-                if (query == "") {
-                  return val;
-                } else {
-                  return val.Name.toLowerCase().includes(query.toLowerCase());
-                  //  && val.Symbol.toLowerCase().includes(query.toLowerCase());
-                }
-              })
-              .map((token, index) => (
-                <div
-                  style={{
-                    padding: "5px 20px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    cursor: "pointer",
-                  }}
-                  key={index}
-                >
-                  <Card
-                    style={{
-                      backgroundColor: "#202020",
-                      width: "90%",
-                      color: "lime",
-                      border: "1px solid #202020",
-                      margin: "0px auto",
-                    }}
-                  >
-                    <Link to={`/${token.Name}/${token.Address}`}>
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        <img
-                          style={{
-                            height: "32px",
-                            width: "32px",
-                            marginRight: "20px",
-                            float: "left",
-                          }}
-                          src={token.Logo}
-                          alt="noLogo"
-                        />
-                        <div style={{ float: "left" }}>
-                          <h4 title="Organization" style={{ color: "lime" }}>
-                            {token.Name}
-                          </h4>
-                          <span
-                            style={{
-                              fontWeight: "600",
-                              fontSize: "15px",
-                              lineHeight: "14px",
-                              color: "lime",
-                              position: "absolute",
-                            }}
-                          >
-                            {token.Symbol}
-                          </span>
-                        </div>
-                        <div>
-                          <span
-                            style={{
-                              fontWeight: "600",
-                              fontSize: "15px",
-                              padding: "20px",
-                              color: "lime",
-                              position: "absolute",
-                              left: "40%",
-                              float: "top",
-                              top: "20%",
-                            }}
-                          >
-                            {token.Type == null ? "--" : token.Type}
-                          </span>
-                          <span
-                            style={{
-                              fontWeight: "600",
-                              fontSize: "15px",
-                              padding: "20px",
-                              color: "lime",
-                              left: "70%",
-                              float: "top",
-                              position: "absolute",
-                              top: "20%",
-                            }}
-                          >
-                            {token.LastPrice == null ? "--" : token.LastPrice}
-                          </span>
-                        </div>
+            .filter((val) => {
+              if (query == "") {
+                return val;
+              } else {
+                return val.Name.toLowerCase().includes(query.toLowerCase());
+                //  && val.Symbol.toLowerCase().includes(query.toLowerCase());
+              }
+            })
+            .map((token, index) => (
+              <div className="cardContainer" key={index}>
+                <Card className="daoCard">
+                  <Link to={`/${token.Name}/${token.Address}`}>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <img className="logo" src={token.Logo} alt="noLogo" />
+                      <div>
+                        <h4 className="name">{token.Name}</h4>
+                        <span className="symbol">{token.Symbol}</span>
                       </div>
-                    </Link>
-                    {watchlist.includes(token.Address) ? (
-                      <StarFilled
-                        style={{ float: "right", top: "10%", fontSize: "125%" }}
-                        onClick={() => removeWatchlist(token.Address)}
-                      />
-                    ) : (
-                      <StarOutlined
-                        style={{ float: "right", top: "10%", fontSize: "125%" }}
-                        onClick={() => addWatchlist(token.Address)}
-                      />
-                    )}
-                  </Card>
-                </div>
-              ))}
+                      <div>
+                        <span className="type">
+                          {token.Type == null ? "--" : token.Type}
+                        </span>
+                        <span className="lastPrice">
+                          {token.LastPrice == null ? "--" : token.LastPrice}
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                  {watchlist.includes(token.Address) ? (
+                    <StarFilled
+                      className="favorites"
+                      onClick={() => removeWatchlist(token.Address)}
+                    />
+                  ) : (
+                    <StarOutlined
+                      className="favorites"
+                      onClick={() => addWatchlist(token.Address)}
+                    />
+                  )}
+                </Card>
+              </div>
+            ))}
       </div>
     </div>
   );
